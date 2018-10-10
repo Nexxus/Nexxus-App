5c4430cd5739c5357ad4f0bb784cf2123a16a83f
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-10-10 14:36:01 +0200

    nexxus images added

--- www/js/userajax.js
+++ www/js/userajax.js
@@ -1,25 +1,35 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        console.log("Status: " + xhr.status);
-        console.log(xhr.response);
 
-        var user = JSON.parse(xhr.response);
-
-        console.log(typeof(user));
-        console.log(user["name"]);
-
-        $("#userName").html(user["name"]);
-    }
-}
-
-xhr.open("GET", "http://localhost:8000/json/get/user", true, "testadmin", "test123");
-xhr.send();
+const url = "http://localhost:8000/";
+const user = "testadmin";
+const pass = "test123";
 
 function connectJson() {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log("Status: " + xhr.status);
+            console.log(xhr.response);
 
+
+            
+        }
+    }
+
+    return xhr;
 }
 
 function getUserInfo() {
+    let xhr = connectJson();
 
+    xhr.open("GET", url+"json/get/user", true, user, pass);
+    xhr.send();
+
+    let user = JSON.parse(xhr.response);
+
+    console.log(typeof(user));
+    console.log(user["name"]);
+
+    $("#userName").html(user["name"]);
 }
+
+getUserInfo();
