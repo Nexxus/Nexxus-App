e80e29da63097ae0283efcad2555e4175f61202b
Ben <ben.wolthuis@gmail.com>
2019-03-20 09:43:01 +0100

    config: changed offeredid 300->11, accepted 301->2

--- www/include/config.js
+++ www/include/config.js
@@ -4,7 +4,7 @@ class UserConfig {
         this.url = localStorage.getItem("domain");
         this.api = this.url;
 
-        this.offeredId  = 300; 
-        this.acceptedId = 301;
+        this.offeredId  = 11; 
+        this.acceptedId = 2;
     }
 }
